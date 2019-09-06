99e1635d9d437e7c999204970706b26b53b990f6
Laila Vinson <lvinson@cerego.com>
2019-09-06 11:29:19 -0700

    Put back CSS attribute name. Name is really not a good name.

--- NUI/Core/NUISettings.m
+++ NUI/Core/NUISettings.m
@@ -218,7 +218,7 @@ static NUISettings *instance = nil;
         fontSize = baseFont ? baseFont.pointSize : [UIFont systemFontSize];
     }
     
-    propertyName = @"font-CERName";
+    propertyName = @"font-name";
     
     if ([self hasProperty:propertyName withClass:className]) {
         NSString *fontName = [self get:propertyName withClass:className];
